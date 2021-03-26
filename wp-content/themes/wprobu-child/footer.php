0e4be8c86b6750a36f746acae2a9816273f0e87b
Alexandru Negoita <alexandru@dekode.no>
2021-03-26 16:40:43 +0200

    Making it simpler.

--- wp-content/themes/wprobu-child/footer.php
+++ wp-content/themes/wprobu-child/footer.php
@@ -19,26 +19,6 @@
 	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
 		<div class="site-info">
 			<div class="powered-by">
 				<?php
